3874355af03f8ff1d2eca59817c025dc943eaf82
Ishna Verma <vdesire2641@gmail.com>
2015-12-28 16:23:50 -0600

    Update t3.py

--- t3.py
+++ t3.py
@@ -2,9 +2,13 @@
 # by 400. write a function that takes a year as a parameter and returns true
 # if the year is a leap year and false otherwise
 # 2.32
+# defining a function leapyear with year as the parameter
 def leapyear(year):
+    # initializing the condition
     if (year % 100 == 0):
         print(" this is century")
+        # there can be if condition inside an if condition
+        # that's fine
         if (year % 4 ==0 and year % 400 == 0):
             print("true")
         else:
@@ -15,5 +19,5 @@ def leapyear(year):
             print("true")
         else:
             print("false")
-
+# calling the function to check the execution
 leapyear(3000)
